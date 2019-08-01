b5a7b480fcf2774fe4d2cdd71a5da67beae5aa06
saddam hussain <saddam.in@gmailo.com>
2019-08-01 05:49:18 -0700

    product_hunt project

--- producthunt/urls.py
+++ producthunt/urls.py
@@ -3,9 +3,12 @@ from django.contrib import admin
 from django.urls import path,include
 from products import views
 from accounts import urls
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('account/',include('accounts.urls'), name='account'),
-]
+    path('product/', include('products.urls'), name='product'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
